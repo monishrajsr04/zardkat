f5d0f877b94b018df117a5277fd953ef959f2a4e
monishrajsr04 <monishrajsr.btech22@rvu.edu.in>
2024-09-27 21:27:57 +0530

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
+import 'dotenv/config'
 import "@nomicfoundation/hardhat-toolbox";
 // https://github.com/projectsophon/hardhat-circom
 import "hardhat-circom";
@@ -22,12 +23,18 @@ const config: HardhatUserConfig = {
       }
     ]
   },
+  networks:{
+  amoy:{
+      url: 'https://rpc-amoy.polygon.technology/',
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  },
   circom: {
     // (optional) Base path for input files, defaults to `./circuits/`
     inputBasePath: "./circuits",
     // (required) The final ptau file, relative to inputBasePath, from a Phase 1 ceremony
     ptau: "powersOfTau28_hez_final_12.ptau",
-    // (required) Each object in this array refers to a separate circuit
+    // (required) Each object in this array refers to a separate circuit55
     circuits: JSON.parse(JSON.stringify(circuits))
   },
 };
